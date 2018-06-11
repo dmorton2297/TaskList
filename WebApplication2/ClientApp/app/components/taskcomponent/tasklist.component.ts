9da12ca9ec8e8b1c49e002a7f1e8d843a2e44e82
Dan Morton <dmorton@wmp.com>
2018-06-11 13:26:29 -0500

    Basic scaffolding and database configuration done

---
+++ WebApplication2/ClientApp/app/components/taskcomponent/tasklist.component.ts
@@ -0,0 +1,32 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'tasklist',
+    templateUrl: './tasklist.component.html'
+})
+export class TaskListComponent {
+    public tasks: Task[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/Data/Tasks').subscribe(result => {
+            this.tasks = result.json() as Task[];
+            console.log(this.tasks);
+        }, error => console.error(error));
+    }
+}
+
+interface Task {
+    Id: number;
+    Name: string;
+    Description: string;
+    Priority: string;
+    Completed: string;
+}
+
+
+interface ResponseModel {
+    Message: string,
+    Status: boolean,
+    Data: object
+}
---
+++ WebApplication2/Controllers/DataController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        [HttpGet("[action]")]
+        public List<Task> Tasks()
+        {
+
+             SqlConnection conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Tasks;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+             SqlCommand comm = conn.CreateCommand();
+             comm.CommandText = "Select * from TaskList";
+             conn.Open();
+
+             SqlDataReader reader = comm.ExecuteReader();
+             List<Task> results = new List<Task>();
+             if (reader.HasRows)
+             {
+                 while (reader.Read())
+                 {
+                     Task entry = new Task
+                     {
+                         Id = reader.GetInt32(0),
+                         Name = reader.GetString(1),
+                         Description = reader.GetString(2),
+                         Priority = reader.GetString(3),
+                         Completed = reader.GetString(4)
+                     };
+                     results.Add(entry);
+                     Debug.Write("Entry retrieved, ID = " + entry.Id + "\n\n");
+                 }
+             }
+
+
+            return results;
+
+        }
+
+        [Serializable]
+        public class Task
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Priority { get; set; }
+            public string Completed { get; set; }
+        }
+
+      
+    }
+}
