60da8b27a5b53c3063ba3a31e43b4a98fa438d71
Dan Morton <dmorton@wmp.com>
2018-06-18 10:55:11 -0500

    Added test case with http request logging

--- WebApplication2/Tests/test1.js
+++ WebApplication2/Tests/test1.js
@@ -2,22 +2,31 @@ import { Selector } from 'testcafe';
 import { RequestMock, RequestLogger } from 'testcafe'
 
 
-const logger = RequestLogger('http://localhost:65065/tasklist');
+
+// Base URL for testing application 
+const createUrl = 'http://localhost:65065/api/Data/Create';
+
+// Logger 
+const logger = RequestLogger(createUrl);
+
+// HTTP Response Mocking
 const mock = RequestMock()
     .onRequestTo('http://localhost:65065/api/Data/GetTasks')
     .respond([{ id: 0, name: 'test', description: 'test description', priority: 'H', completed: 'N', category: 4 }])
     .onRequestTo('http://localhost:65065/api/Data/GetTask/*')
-    .respond({ id: 0, name: 'test', description: 'test description', priority: 'H', completed: 'N', category: 4 })
+    .respond({ id: 0, name: 'test', description: 'test description', priority: 'H', completed: 'N', category: 4 });
 
-
-fixture`Getting Started`
+// Fixture to define starting point any request hooks
+// Each test script must have a fixture
+fixture`TaskList Test Main`
     .page`http://localhost:65065/home`
     .requestHooks(logger);
 
 // Navigation Test
+// This test demonstrates simple UI Navigation with TestCafe
 test
     .requestHooks(mock)
-    ('Navigation Test 1', async t => {
+    ('Navigation Test 1', async t => { 
     await t
         .click('#tasklist-link')
 
@@ -26,6 +35,7 @@ test
 });
 
 // Data loading test
+// This test shows how request mocking can be used
 test
     .requestHooks(mock)
     ('Tasklist Test 1', async t => {
@@ -37,6 +47,8 @@ test
 
 
 // Test if details of selected task transfer to update view
+// This test looks for information to be loaded into a detial view
+// Also utilizes Http mocking
 test
     .requestHooks(mock)
     ('Update Task Test 1', async t => {
@@ -47,4 +59,26 @@ test
 
     });
 
+// Test adding a task to the database 
+// This test utilizes HTTP request logging
+const categorySelect = Selector('#category');
+const categoryOption = categorySelect.find('option');
+test
+    .requestHooks(mock)
+    .requestHooks(logger)
+    ('Add Task Test 1', async t => {
+        await t
+            .navigateTo('http://localhost:65065/addtask')
+            .wait(100)
+            .click('#name')
+            .typeText('#name', 'add test 1')
+            .click('#priority')
+            .typeText('#priority', 'L')
+            .click('#description')
+            .typeText('#description', 'add test desc 1')
+            .click(categorySelect)
+            .click(categoryOption.withText('Family'))
+            .click('#submitbutton')
+            .expect(logger.contains(record => record.response.statusCode === 200)).ok();
 
+    });
